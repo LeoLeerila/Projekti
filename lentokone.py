92e87875cc6413022d592693f31d5b4a276e7349
LeoLeerila <leo.leerila@metropolia.fi>
2025-02-25 15:40:29 +0200

    lentokone.py päivittää pelaajan co2_consumed

--- lentokone.py
+++ lentokone.py
@@ -1,16 +1,19 @@
 from geopy import distance
+from tietokanta import paivitaPelaajanSijainti, paivitaPelaajanCo2
 
 #lentokoneen nopeus km/h
-nopeus = 250
+nopeus = 933
 #co_2 päästöt
-co2 = 1
+co2 = 8
 
 def lenna(maaranpaa, sijainti):
     #lasketaan matkan pituus ja kuinka kauan siinä kestää
     matkanpituus = distance.distance(maaranpaa, sijainti).km
     kesto = matkanpituus / nopeus
+    co2Lennolta = co2 * kesto
     #päivitetään tietokaanta
-
+    #paivitaPelaajanSijainti(maaranpaa)
+    paivitaPelaajanCo2(co2Lennolta)
     print(f"matkan pituus: {matkanpituus} km, joka kestää: {kesto} tuntia")
     pass
 
