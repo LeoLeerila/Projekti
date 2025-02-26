fd0228c1988be943f354f1915e20867e8600ce99
LeoLeerila <leo.leerila@metropolia.fi>
2025-02-26 14:43:38 +0200

    poistettu ylimääräiset kursori = yhteys.cursor() rivit

--- tietokanta.py
+++ tietokanta.py
@@ -8,12 +8,12 @@ yhteys = mysql.connector.connect(
     password='pelaajansalasana',
     autocommit=True
 )
+kursori = yhteys.cursor()
 
 def haePelaajanTiedot(pelaajanId):
     #haetut pelaajan arvot ovat järjestykseesä
     #id, co2_consumed, co2_budget, location, screen_name, time
     sql = f'SELECT * FROM game WHERE id = "{pelaajanId}"'
-    kursori = yhteys.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()[0]
     return tulos
@@ -22,22 +22,23 @@ def haeMaanTiedot(haettavaTieto, rajausTieto, rajausTiedonArvo):
     #hae maan tiedot
     #mahdollisia haettavaTieto ja rajausTieto arvoja ovat
     #iso_country, name, continent, wikipedia_link, keywords
-    sql = f'SELECT {haettavaTieto} FROM country WHERE {rajausTieto} = {rajausTiedonArvo}'
-    kursori = yhteys.cursor()
+    sql = f'SELECT {haettavaTieto} FROM country WHERE {rajausTieto} = "{rajausTiedonArvo}"'
     kursori.execute(sql)
     tulos = kursori.fetchall()[0]
     return tulos
 
-def haeMaanLentokentat(maa):
-    sql = f'SELECT name FROM airport WHERE ((type = "small_airport") OR (type = "medium_airport") OR (type = "large_airport")) AND iso_country = "{maa}"'
-    kursori = yhteys.cursor()
+def haeMaanLentokentat(iso_country):
+    #((type = "small_airport") OR (type = "medium_airport") OR (type = "large_airport"))
+    sql = f'SELECT name FROM airport WHERE type = "large_airport" AND iso_country = "{iso_country}"'
     kursori.execute(sql)
-    tulos = kursori.fetchall()[0]
+    tulos = kursori.fetchall()
     return tulos
 
-def haeLentokentanSijainti(lentokentanIdent):
-    sql = f'SELECT latitude_deg, longitude_deg FROM airport WHERE ident = "{lentokentanIdent}"'
-    kursori = yhteys.cursor()
+def haeLentokentanTiedot(haettavaTieto, rajausTieto, rajausTiedonArvo):
+    #hae lentokentan tiedot
+    #mahdollisia haettavaTieto ja rajausTieto arvoja ovat
+    #id, ident, type, name, latitude_deg, longitude_deg, elevation_ft, continent, iso_country, iso_region, municipality, scheduled_service, gps_code, iata_code, local_code, home_link, wikipedia_link, keywords
+    sql = f'SELECT {haettavaTieto} FROM airport WHERE {rajausTieto} = "{rajausTiedonArvo}"'
     kursori.execute(sql)
     tulos = kursori.fetchall()[0]
     return tulos
@@ -52,13 +53,12 @@ def paivitaPelaajanTiedot(pelaajanId, paivitettavaTieto, tiedonArvo):
     #mahdollisia päivityksiä pelaajan tietoihin ovat
     #id, co2_consumed, co2_budget, location, screen_name, time
     sql = f'UPDATE game SET {paivitettavaTieto} = "{tiedonArvo}" WHERE id = "{pelaajanId}";'
-    kursori = yhteys.cursor()
     kursori.execute(sql)
     pass
 
-#lentokentta = etsiMaanLentokentat("KP")
-#print(lentokentta)
-#paivitaPelaajanCo2(-160)
-#paivitaPelaajanSijainti("EFHK")
-#print(haeLentokentanSijainti("EFHK"))
-print(haeMaanTiedot("name", ""))
+
+print(haePelaajanTiedot(1))
+print(haeMaanTiedot("name", "iso_country", "FI"))
+print(haeMaanLentokentat("US"))
+print(haeLentokentanTiedot("name, type, municipality", "ident", "EFHK"))
+print(haeLentokentanTiedot("latitude_deg, longitude_deg", "ident", "EFHK"))
