79364ba945bba85c4fa324e72872094acbe657f2
rulaaja <roope.rajala@metropolia.fi>
2025-03-04 15:29:45 +0200

    jou

---
+++ aloitus.py
@@ -0,0 +1,12 @@
+print("""\
+
+
+      __|__
+*---o--(_)--o---*                _  _
+                               ( `   )_
+   MIEHEN SALAINEN MATKA    (    )    `)
+      THAIMAA             (_   (_ .  _) _)                       
+
+   """)
+aloitus = input("Paina mitä vain aloittaaksesi: ")
+print("Sinun täytyy matkustaa Thaimaaseen ilman, että ylität CO2-budjettisi.")
