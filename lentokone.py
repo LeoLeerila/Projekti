4473b755cc6b9d9255c27d7e4519983e933375bc
LeoLeerila <leo.leerila@metropolia.fi>
2025-03-04 13:01:55 +0200

    lentokone.py lenna funktio päivitetty

--- lentokone.py
+++ lentokone.py
@@ -1,21 +1,25 @@
 from geopy import distance
-from tietokanta import paivitaPelaajanSijainti, paivitaPelaajanCo2
+from tietokanta import paivitaPelaajanTiedot, haeLentokentanTiedot
 
 #lentokoneen nopeus km/h
 nopeus = 933
 #co_2 päästöt
 co2 = 8
 
-def lenna(maaranpaa, sijainti):
+def lenna(maaranpaa, sijainti, paivitaPelaaja):
+    #haetaan sijainnin ja määränpään koordinaatit
+    maaranpaaKoordinaatit = haeLentokentanTiedot("latitude_deg, longitude_deg", "ident", maaranpaa)
+    sijaintiKoordinaatit = haeLentokentanTiedot("latitude_deg, longitude_deg", "ident", sijainti)
     #lasketaan matkan pituus ja kuinka kauan siinä kestää
-    matkanpituus = distance.distance(maaranpaa, sijainti).km
+    matkanpituus = distance.distance(maaranpaaKoordinaatit, sijaintiKoordinaatit).km
     kesto = matkanpituus / nopeus
     co2Lennolta = co2 * kesto
-    #päivitetään tietokaanta
-    #paivitaPelaajanSijainti(maaranpaa)
-    paivitaPelaajanCo2(co2Lennolta)
+    if paivitaPelaaja == 1:
+        #päivitetään tietokaanta
+        paivitaPelaajanTiedot(1, "location", maaranpaa)
+        paivitaPelaajanTiedot(1, "co2_consumed", co2Lennolta)
     print(f"matkan pituus: {matkanpituus} km, joka kestää: {kesto} tuntia")
     pass
 
 #testaus
-#lenna((60.3172,24.963301),(60.6544,24.8811))
+#lenna("EFHK", "CKQ8", 0)
