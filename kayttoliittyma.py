5c6fbdbcfc4fa2e80e21b74ea0c8efa6579fe068
Rene <rene@karkkainen.net>
2025-03-04 16:04:32 +0200

    AA

--- kayttoliittyma.py
+++ kayttoliittyma.py
@@ -1,4 +1,11 @@
 import inquirer
+from tietokanta import laskeLennonPituus
+
+lentokentat = laskeLennonPituus((53.2400016784668, 50.375), "EU")
+
+for lentokentta in lentokentat:
+    print(lentokentta["maa"])
+
 questions = [
   inquirer.List('size',
                 message="What size do you need?",
