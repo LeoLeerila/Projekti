6e2f66e302b94a38b916d89a531255f36a62cac3
EliasKko <eliaskko@metropolia.fi>
2025-03-05 12:46:07 +0200

    sql testi tiedosto tehtäville

--- PeliTehtavat.py
+++ PeliTehtavat.py
@@ -1,27 +1,17 @@
-import random
+import mysql.connector
 
+yhteys = mysql.connector.connect(
+    host='localhost',
+    port= 3306,
+    database='flight_game_projekti',
+    user='pelaaja',
+    password='pelaajansalasana',
+    autocommit=True
+)
+kursori = yhteys.cursor()
 
-randomLuku = random.randint(1,4)
-def tehava(randomLuku):
-    if randomLuku == 1:
-        kysy = int(input("Mikä on 1+1? "))
-        if kysy == 2:
-            print("oikein")
-
-    elif randomLuku == 2:
-        kysy = int(input("Mikä on 1+2? "))
-        if kysy == 3:
-            print("oikein")
-
-    elif randomLuku == 3:
-        kysy = int(input("Mikä on 1+3? "))
-        if kysy == 4:
-            print("oikein")
-
-    elif randomLuku == 4:
-        kysy = int(input("Mikä on 1+4? "))
-        if kysy == 5:
-            print("oikein")
-
-
-tehava(randomLuku)
+def annakysymys():
+    sql = f'SELECT * FROM tehtavat WHERE kysymysnumero = "{1}"'
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    return tulos
--- tehtava_testi.sql
+++ tehtava_testi.sql
@@ -5,10 +5,12 @@ DROP TABLE IF EXISTS `tehtavat`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tehtavat`
+  `kysymysnumero` int(20) NOT NULL,
   `kysymys_vaihtoehdot` varchar(40) NOT NULL,
   `vastaus_vaihtoehdot` varchar(40) DEFAULT NULL,
   `vastaukset` varchar(40) DEFAULT NULL,
-
+    REFERENCES `tehtavat`
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -17,6 +19,7 @@ CREATE TABLE `tehtavat`
 
 LOCK TABLES `tehtavat` WRITE;
 /*!40000 ALTER TABLE `game` DISABLE KEYS */;
-INSERT INTO `tehtavat` VALUES ('mikä on 1+1?', '2,5,9,1', '2');
+INSERT INTO `tehtavat` VALUES ('1', 'miksi matkaan thaimaaseen?', 'uuden vaimon takia, äijien bilereissun takia,' ||
+                                                                  'kauniin kulttuurin takia', 'kauniin kulttuurin takia');
 /*!40000 ALTER TABLE `game` ENABLE KEYS */;
 UNLOCK TABLES;
