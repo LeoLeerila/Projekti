f700fff2c4b0212562a25d73158a5fbfd54a90c6
Rene <rene@karkkainen.net>
2025-03-06 21:37:13 +0200

    laskeLennonPituus function korjaus

--- kayttoliittyma.py
+++ kayttoliittyma.py
@@ -44,6 +44,7 @@ def valitseSeuraavaLentokentta(location):
   print("You selected:", answers["maa"][0])
 
   lento = lenna(answers["maa"][0], haePelaajanTiedot(1)["location"], 1, 1)
+  print(lento)
 
   return answers["maa"][0]
 
