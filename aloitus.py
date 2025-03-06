a253fb5fe8ee81f1c8ed96f33a7ffd671038feb3
rulaaja <roope.rajala@metropolia.fi>
2025-03-06 09:24:44 +0200

    jou

--- aloitus.py
+++ aloitus.py
@@ -1,12 +1,36 @@
-print("""\
+import inquirer
+
+valinnat = ["Aloita peli", "Lopeta peli"]
+
+def aloitus():
+    print("""\
 
 
-      __|__
-*---o--(_)--o---*                _  _
-                               ( `   )_
-   MIEHEN SALAINEN MATKA    (    )    `)
-      THAIMAA             (_   (_ .  _) _)                       
+          __|__
+    *---o--(_)--o---*                _  _
+                                   ( `   )_
+       MIEHEN SALAINEN MATKA    (    )    `)
+          THAIMAA             (_   (_ .  _) _)                       
 
-   """)
-aloitus = input("Paina mitä vain aloittaaksesi: ")
+       """)
+    aloitus = input("Paina mitä vain aloittaaksesi: ")
+
+    questions = [
+        inquirer.List('valinta',
+                      message="Valitse:",
+                      choices=valinnat,  # Use the dynamically generated list
+                      ),
+    ]
+
+
+    answers = inquirer.prompt(questions)
+
+    if answers['valinta'] == "Aloita peli":
+        print("Aloitetaan peli! Matka Thaimaahan alkaa...")
+
+    else:
+        print("Peli lopetetaan.")
+        exit()
+
 print("Sinun täytyy matkustaa Thaimaaseen ilman, että ylität CO2-budjettisi.")
+aloitus()
