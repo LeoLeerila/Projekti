5c9a0d4f90124cb26ef4e28f4629d0352cebb730
Rene <rene@karkkainen.net>
2025-03-06 21:22:34 +0200

    +

--- kayttoliittyma.py
+++ kayttoliittyma.py
@@ -1,7 +1,9 @@
 import inquirer
-from tietokanta import haeMaanTiedot, haePelaajanTiedot, haeMaanLentokentat, haePelaajanNykyinenMaa
+from tietokanta import haeMaanTiedot, haePelaajanTiedot, haeMaanLentokentat, haePelaajanNykyinenMaa, paivitaPelaajanTiedot
 from lentokone import laskeLennonPituus, lenna
 import chalk
+import random
+
 
 def lentokentat(lahtoSijainti):
   # Get the list of airports
@@ -10,23 +12,27 @@ def lentokentat(lahtoSijainti):
   maat = []
   for lentokentta in lentokentat:
       maa_tiedot = haeMaanTiedot("name", "iso_country", lentokentta["maa"])
-      if maa_tiedot and len(maa_tiedot[0]) > 0:  # Ensure data exists
-          if round(lentokentta["co2Lennolta"], 2) <= haePelaajanTiedot(1)["co2_budget"]:
-            #if haePelaajanTiedot(1)["location"] != haeMaanLentokentat(lentokentta["maa"])[0]:
-            maat.append((f"{maa_tiedot[0][0]} {chalk.red(f"-{round(lentokentta["co2Lennolta"], 2)} kg CO₂")}", haeMaanLentokentat(lentokentta["maa"])))  # Extract the first name
+      
+      if maa_tiedot and len(maa_tiedot[0]) > 0 and haeMaanLentokentat(lentokentta["maa"]):  # Ensure data exists
+          if round(lentokentta["co2Lennolta"], 2) <= haePelaajanTiedot(1)["co2_budget"] - haePelaajanTiedot(1)["co2_consumed"]:
+            if haePelaajanTiedot(1)["location"] != haeMaanLentokentat(lentokentta["maa"])[0]:
+              maat.append((f"{maa_tiedot[0][0]} {chalk.red(f"-{round(lentokentta["co2Lennolta"], 2)} kg CO₂")}", haeMaanLentokentat(lentokentta["maa"])))
+  print(f"Calculated routes to {len(lentokentat)} countries.")
   return maat
    
 
 def valitseSeuraavaLentokentta(location):
   pelaajanTiedot = haePelaajanTiedot(1)
   print("You are in", chalk.green(haePelaajanNykyinenMaa(pelaajanTiedot["location"])))
-  print("You can still emit a total of", pelaajanTiedot["co2_budget"]-pelaajanTiedot["co2_consumed"], "kg CO₂")
+  print("You can emit a total of", chalk.green(pelaajanTiedot["co2_budget"]-pelaajanTiedot["co2_consumed"]), "kg CO₂")
 
   maat = lentokentat(location)
+  if not maat:
+     return
   # Create the question with dynamic choices
   questions = [
       inquirer.List('maa',
-                    message="Choose a country:",
+                    message=f"Where would you like to fly? ({len(maat)})",
                     choices=maat,  # Use the dynamically generated list
                 ),
   ]
@@ -39,4 +45,23 @@ def valitseSeuraavaLentokentta(location):
 
   print(lenna(answers["maa"][0], haePelaajanTiedot(1)["location"], 1, 1))
 
-valitseSeuraavaLentokentta(haePelaajanTiedot(1)["location"])
+  return answers["maa"][0]
+
+def testiTehtava():
+  questions = [
+      inquirer.List('tehtava',
+                    message="Choose a country:",
+                    choices=[("Maapallo on litteä", 0), ("Maapallo on pyöreä", 1)]
+                ),
+  ]
+
+  answers = inquirer.prompt(questions)
+  print("You selected:", answers["tehtava"])
+  if answers["tehtava"] == 1:
+      print("Correct answer!")
+      pelaajanTiedot = haePelaajanTiedot(1)
+      paivitaPelaajanTiedot(1, "co2_budget", (pelaajanTiedot["co2_budget"]+random.randint(200, 500)))
+  else:
+     print(chalk.red("Wrong answer :("))
+
+#valitseSeuraavaLentokentta(haePelaajanTiedot(1)["location"])
---
+++ kayttoliittyma.test.py
@@ -0,0 +1,79 @@
+from tietokanta import haePelaajanTiedot, nollaaPelaajanTiedot
+from kayttoliittyma import valitseSeuraavaLentokentta, testiTehtava
+import os
+import time
+
+win_frames = [
+    r"""
+     \O/  
+      |   
+     / \  
+    """,
+    r"""
+      O   
+     /|\  
+     / \  
+    """,
+    r"""
+     /O\  
+      |   
+     / \  
+    """,
+    r"""
+      O/  
+     /|   
+     / \  
+    """
+]
+
+loss_frames = [
+    r"""
+      _______  _______  _______ 
+     (  ____ \(  ___  )(  ____ \
+     | (    \/| (   ) || (    \/
+     | (_____ | |   | || (_____ 
+     (_____  )| |   | |(_____  )
+           ) || |   | |      ) |
+     /\____) || (___) |/\____) |
+     \_______)(_______)\_______)
+    
+            GAME OVER
+    """,
+    r"""
+      ____   ___  ____  
+     / ___) / __)(  _ \ 
+     \___ \( (__  )   / 
+     (____/ \___)(__\_)
+    
+            GAME OVER
+    """
+]
+
+pelaajanTiedot = haePelaajanTiedot(1)
+#id, co2_consumed, co2_budget, location, screen_name, time, km_total
+
+peliKaynnissa = 1
+
+if pelaajanTiedot["km_total"] == 0:
+    #pelaajalle annetaan pelin aluksi aloitus.py alkuinfo
+    from aloitus import aloitus
+    #jos pelaaja haluaa pelaajalle annetaan tutoriaali lentokoneella lentämisestä ja tehtävistä
+
+
+while peliKaynnissa == 1:
+    #pelaaja valitsee seuraavan maan johon lentää
+    maa = valitseSeuraavaLentokentta(pelaajanTiedot["location"])
+    #print(maa)
+
+    #jos pelaajan co2_consumed on liian suuri eikä hän voi lentää uuteen maahan peli päättyy
+    if pelaajanTiedot["co2_consumed"] >= pelaajanTiedot["co2_budget"] or not maa:
+        peliKaynnissa = 0
+        print("Hävisit pelin :(")
+        nollaaPelaajanTiedot(1)
+        exit()
+    #jos pelaaja pääsee thaimaahan pelaaja voittaa pelin ja peli päättyy
+    if maa == "EFHK":
+        print("Voitit pelin!")
+
+    if maa:
+        testiTehtava()
