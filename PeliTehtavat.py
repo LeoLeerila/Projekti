c3149f609a433f5c60212e75db004867722ccf98
EliasKko <eliaskko@metropolia.fi>
2025-03-06 18:17:39 +0200

    pelitehtätävien muutosta

--- PeliTehtavat.py
+++ PeliTehtavat.py
@@ -1,9 +1,3 @@
-import os
-import sys
-from pprint import pprint
-
-sys.path.append(os.path.realpath("."))
-
 import inquirer
 
 from tietokanta import annakysymys
@@ -15,16 +9,15 @@ from tietokanta import annakysymysVaihtoehto3
 oikeavastaus = annavastaus(1)[0][0]
 kysymys = annakysymys(1)[0]
 print(kysymys)
-print(oikeavastaus)
+lista = [annakysymysVaihtoehto1(1)[0], annakysymysVaihtoehto2(1)[0], annakysymysVaihtoehto3(1)[0]]
 questions = [
   inquirer.List('interests',
                     message= '"Tää on kyl paha..."',
-                    carousel=True,
-                    choices=[annakysymysVaihtoehto1(1)[0], annakysymysVaihtoehto2(1)[0], annakysymysVaihtoehto3(1)[0]])]
-#answer = inquirer.prompt(questions)
+                    choices=lista)]
+answer = inquirer.prompt(questions)
 #pprint(answer)
-vastaus = input("kirjoita vastaus: ")
-if vastaus == oikeavastaus:
+
+if answer == oikeavastaus:
     print("Kyl se on")#, answer["Tää"])
 
 
