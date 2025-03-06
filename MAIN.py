d4cd103c82e699b359a5acebab0d168c6ffc58f2
LeoLeerila <leo.leerila@metropolia.fi>
2025-03-06 11:15:07 +0200

    pelin perus rakenteen suunnitelma

--- MAIN.py
+++ MAIN.py
@@ -1,4 +1,6 @@
 from tietokanta import haePelaajanTiedot
+from aloitus import aloitus
+from kayttoliittyma import valitseSeuraavaLentokentta
 
 pelaajanTiedot = haePelaajanTiedot(1)
 #id, co2_consumed, co2_budget, location, screen_name, time, km_total
@@ -11,3 +13,21 @@ pelaajanTiedot = {
     "time": pelaajanTiedot[5],
     "km_total": pelaajanTiedot[6]
 }
+
+peliKaynnissa = 1
+
+if pelaajanTiedot["km_total"] == 0:
+    #pelaajalle annetaan pelin aluksi aloitus.py alkuinfo
+    aloitus()
+    #jos pelaaja haluaa pelaajalle annetaan tutoriaali lentokoneella lentämisestä ja tehtävistä
+
+
+while peliKaynnissa == 1:
+    #pelaaja valitsee seuraavan maan johon lentää
+    valitseSeuraavaLentokentta()
+    #pelaaja tekee tehtävän
+
+    #jos pelaajan co2_consumed on liian suuri eikä hän voi lentää uuteen maahan peli päättyy
+    if pelaajanTiedot["co2_consumed"] >= pelaajanTiedot["co2_budget"]:
+        peliKaynnissa = 0
+    #jos pelaaja pääsee thaimaahan pelaaja voittaa pelin ja peli päättyy
