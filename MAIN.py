c8ef0de09980fdd61e76018af1c16f06f1e82217
LeoLeerila <leo.leerila@metropolia.fi>
2025-03-06 11:26:43 +0200

    pelaajanTiedot ei muuta duplea sanakirjaksi koska saa suoraan sanakirjan

--- MAIN.py
+++ MAIN.py
@@ -4,15 +4,6 @@ from kayttoliittyma import valitseSeuraavaLentokentta
 
 pelaajanTiedot = haePelaajanTiedot(1)
 #id, co2_consumed, co2_budget, location, screen_name, time, km_total
-pelaajanTiedot = {
-    "id": pelaajanTiedot[0],
-    "co2_consumed": pelaajanTiedot[1],
-    "co2_budget": pelaajanTiedot[2],
-    "location": pelaajanTiedot[3],
-    "screen_name": pelaajanTiedot[4],
-    "time": pelaajanTiedot[5],
-    "km_total": pelaajanTiedot[6]
-}
 
 peliKaynnissa = 1
 
@@ -24,10 +15,11 @@ if pelaajanTiedot["km_total"] == 0:
 
 while peliKaynnissa == 1:
     #pelaaja valitsee seuraavan maan johon lentää
-    valitseSeuraavaLentokentta()
+    valitseSeuraavaLentokentta(pelaajanTiedot["location"])
     #pelaaja tekee tehtävän
 
     #jos pelaajan co2_consumed on liian suuri eikä hän voi lentää uuteen maahan peli päättyy
     if pelaajanTiedot["co2_consumed"] >= pelaajanTiedot["co2_budget"]:
         peliKaynnissa = 0
+        print("peli päättyi")
     #jos pelaaja pääsee thaimaahan pelaaja voittaa pelin ja peli päättyy
