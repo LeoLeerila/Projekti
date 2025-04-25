86ef769cb75b5d4192529f0698a770eb640d329a
LeoLeerila <leo.leerila@metropolia.fi>
2025-04-25 11:11:45 +0300

    lopetus funktiot (ei käytössä)

--- palvelin.py
+++ palvelin.py
@@ -7,32 +7,36 @@ from PeliTehtavat import kysymys
 
 app = Flask(__name__)
 
-#tietokannan funktiot funcHaePelaajanTiedot funcNollaaPelaajanTiedot
-#app.add_url_rule("/tietokanta/haePelaajanTiedot/<int:pelaajanID>", endpoint="PelaajanTiedot")
-#app.add_url_rule("/tietokanta/nollaaPelaajanTiedot/<int:pelaajanID>", endpoint="PelaajanTiedot")
-#app.add_url_rule("/tietokanta/paivitaPelaajanTiedot/<int:pelaajanID>,<paivitettavaTieto>,<tiedonArvo>", endpoint="PelaajanTiedot")
-
-@app.route("/PelaajanTiedot/hae/")
+#Tietokannan funktiot
+@app.route("/PelaajanTiedot/hae/")#http://127.0.0.1:3000/PelaajanTiedot/hae/?pelaajanID=1
 def hae():
     args = request.args
     vastaus = haePelaajanTiedot(args.get("pelaajanID"))
     return vastaus
 
-@app.route("/PelaajanTiedot/nollaa/")
+@app.route("/PelaajanTiedot/nollaa/")#http://127.0.0.1:3000/PelaajanTiedot/nollaa/?pelaajanID=1
 def nollaa():
     args = request.args
     nollaaPelaajanTiedot(args.get("pelaajanID"))
     return f"pelaajan {args.get("pelaajanID")} tiedot päivitetty"
 
-@app.route("/PelaajanTiedot/paivita/")
+@app.route("/PelaajanTiedot/paivita/")#http://127.0.0.1:3000/PelaajanTiedot/paivita/?pelaajanID=1&paivitettavaTieto=co2_consumed&tiedonArvo=100
 def paivita():
     args = request.args
     print(args)
     paivitaPelaajanTiedot(args.get("pelaajanID"), args.get("paivitettavaTieto"), args.get("tiedonArvo"))
     return f"Pelaajan {args.get("pelaajanID")} tieto {args.get("paivitettavaTieto")} muutettu {args.get("tiedonArvo")}"
 
+#Lopetus funktiot
+@app.route("/Lopetus/voitto/")
+def Lopetusvoitto():
+    #voitto()
+    return "Pelaaja voitti pelin"
 
-
+@app.route("/Lopetus/havio/")
+def Lopetushavio():
+    #havio()
+    return "Pelaaja hävisi pelin"
 
 if __name__ == "__main__":
     app.run(use_reloader=True, host="127.0.0.1", port=3000)
