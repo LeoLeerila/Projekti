b04a3f1892ceb1488a8063834d817fcbb9601836
EliasKko <eliaskko@metropolia.fi>
2025-04-26 15:37:26 +0300

    Terminalia eteenpäin + cors paska palvelimeen

--- Terminal.js
+++ Terminal.js
@@ -1,28 +1,57 @@
 'use strict';
 
-async function HaeLentokenttä() {
-  Terminal.innerHTML = ``;
-  try {
-    const response = await fetch('http://127.0.0.1:3000/PelaajanTiedot/hae/?pelaajanID=1');    // starting data download, fetch returns a promise which contains an object of type 'response'
-    const jsonData = await response.json();
-    console.log(jsonData)
-    for (const [key, value] of Object.entries(jsonData)) {
-      let Data = document.createElement('p')
-      Data.innerHTML = [key, value];
-      Terminal.appendChild(Data)
+const Terminal = document.querySelector('#terminal')
+const Home = document.querySelector('#homescreen')
 
-    }
+let pelaajainfo = false;
+let athome = true;
 
+async function HaePelaajantiedot() {
+  if (pelaajainfo) {
+    Terminal.innerHTML = ``;
+    Home.innerHTML = ``;
+    try {
+      const response = await fetch('http://127.0.0.1:3000/PelaajanTiedot/hae/?pelaajanID=1');
+      const jsonData = await response.json();
+      console.log(jsonData)
+      for (const [key, value] of Object.entries(jsonData)) {
+        let Data = document.createElement('p')
+        Data.innerHTML = [key, value];
+        Terminal.appendChild(Data)
 
-  } catch (error) {
+      }
+      const Back = document.createElement('button')
+      Back.innerHTML = `Back`
+      Back.addEventListener("click", function() {
+          pelaajainfo = false;
+          athome = true;
+          console.log('return')
+          HomeScreen()
+      });
+    Terminal.appendChild(Back)
+    } catch (error) {
         console.log(error.message);
     }
 
+  }
+  else {
+    pelaajainfo = false;
+    athome = true;
+  }
 }
-const Terminal = document.querySelector('#terminal')
-const Fly = document.createElement('button')
-Fly.innerHTML = `Pelaaja`
-Fly.addEventListener("click", function() {
-  HaeLentokenttä();
-});
-Terminal.appendChild(Fly)
+
+function HomeScreen () {
+  if (athome) {
+    Terminal.innerHTML = ``;
+    Home.innerHTML = `Terminal`
+    const Fly = document.createElement('button')
+    Fly.innerHTML = `Pelaaja`
+    Fly.addEventListener("click", function() {
+      pelaajainfo = true;
+      athome = false;
+      HaePelaajantiedot()
+    });
+    Terminal.appendChild(Fly)
+  }
+}
+HomeScreen()
