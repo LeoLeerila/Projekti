227d42e5e79ffa1be30c3fc2e022c6c611cf1913
EliasKko <eliaskko@metropolia.fi>
2025-04-29 16:52:48 +0300

    Terminalia eteenpäin

--- Terminal.js
+++ Terminal.js
@@ -1,8 +1,10 @@
 'use strict';
-
+//const response1 = await fetch('http://127.0.0.1:3000/Lentokentta/valitse/?pelaajanID=1')//
+//const jsonData1 = await response1.json();//
 const Terminal = document.querySelector('#terminal')
 const Home = document.querySelector('#homescreen')
 
+let lentoinfo = false;
 let pelaajainfo = false;
 let athome = true;
 
@@ -41,14 +43,24 @@ async function HaePelaajantiedot() {
 }
 
 async function ValitseLkenttä() {
-  Terminal.innerHTML = ``;
-  Home.innerHTML = ``;
-  try {
-    const response = await fetch('');
-    const jsonData = await response.json();
-    console.log(jsonData)
-  } catch (error) {
-    console.log(error.message);
+  if (lentoinfo) {
+    Terminal.innerHTML = ``;
+    Home.innerHTML = ``;
+    try {
+      const response = await fetch(
+          'http://127.0.0.1:3000/Lentokentta/vaihtoehdot/?pelaajanID=1');
+      const jsonData = await response.json();
+      console.log(jsonData)
+      let Data = document.createElement('div');
+      for (let i = 0; i < jsonData.lentokenttaLista.length; i++) {
+        let P = document.createElement('p')
+        P.innerHTML = jsonData.lentokenttaLista[i]
+        Data.appendChild(P)}
+      Terminal.appendChild(Data)
+
+    } catch (error) {
+      console.log(error.message);
+  }
   }
 
 }
@@ -59,12 +71,20 @@ function HomeScreen () {
     Home.innerHTML = `Terminal`
     const Fly = document.createElement('h2')
     Fly.innerHTML = `Pelaaja`
+    const Playeri = document.createElement('h2')
+    Playeri.innerHTML = `Lennä`
     Fly.addEventListener("click", function() {
       pelaajainfo = true;
       athome = false;
       HaePelaajantiedot()
     });
     Terminal.appendChild(Fly)
+    Playeri.addEventListener("click", function() {
+      lentoinfo = true;
+      athome = false;
+      ValitseLkenttä()
+    });
+    Terminal.appendChild(Playeri)
   }
 }
 HomeScreen()
